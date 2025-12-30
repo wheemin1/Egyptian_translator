cdd9e3a58342630c73e342554b7b7934ef994b7e
hmkin <wheemin1@gmail.com>
2025-12-30 15:28:04 +0900

    chore: add UTM params to rune banner link

--- src/constants/externalLinks.ts
+++ src/constants/externalLinks.ts
@@ -1,1 +1,2 @@
-export const VIKING_RUNE_CONVERTER_URL = "https://viking-rune-converter.netlify.app/";
+export const VIKING_RUNE_CONVERTER_URL =
+	"https://viking-rune-converter.netlify.app/?utm_source=egypt_translator&utm_medium=banner&utm_campaign=cross_promo";
